f7c944556b3084216526d61759ac17c0d58aca14
Adaś <adampekul@gmail.com>
2022-12-27 01:36:41 +0100

    rgbToHex.js console.log correction

--- 5kyu/rgbToHex.js
+++ 5kyu/rgbToHex.js
@@ -30,6 +30,4 @@ console.log(rgb(0, 0, 0)); // 000000
 console.log(rgb(0, 0, -20)); // 000000
 console.log(rgb(300, 255, 255)); // FFFFFF
 console.log(rgb(173, 255, 47)); // ADFF2F
-console.log(rgb(269, 110, 3)); // ADFF2F
-// FF6E30
-// FF6E03
+console.log(rgb(269, 110, 3)); // FF6E30
